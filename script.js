d6ec4698c8ac053db640aaa980060d712a2cf2b1
Shahiel12 <shahiel662@gmail.com>
2023-02-12 12:53:40 +0200

    final

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ number.forEach((button) => {
 })
 
 back.addEventListener('click', () => {
-    backSpace()
+    backSpace();
 })
 
 decimal.addEventListener('click', () => {
@@ -40,15 +40,13 @@ operator.forEach((button) => {
         input.value = inputstring;
         operand = button.value;
         operand2.push(operand);
-        console.log(operand2)
-        console.log(numbers)
         document.getElementById("decimal").disabled = false;
         num = '';
     })
 })
 
 clearbtn.addEventListener('click', () => {
-    clear()
+    clear();
 })
 
 equal.addEventListener('click', () => {    
@@ -56,7 +54,6 @@ equal.addEventListener('click', () => {
         clear();
         input.value = "No Input";
     }else{
-        console.log(numString)
         for(let j = 0; j < numString.length; j++){
             num = numString.split("T");
         }
@@ -67,14 +64,12 @@ equal.addEventListener('click', () => {
             let num2 = num[i+1];
             if(num2 == 0){
                 input.value = "Can't be done";
-                console.log(input.value)
             }else{
                 let operand1 = operand2[j];
                 operate(operand1,num1,num2);
                 num.splice(i+1,1,operate(operand1,num1,num2));
                 j++;
                 input.value = inputstring + " = " + parseFloat(num[num.length - 1]).toFixed(2); 
-                console.log(num)
             }
         }
         document.getElementById("decimal").disabled = false;
@@ -121,14 +116,12 @@ function backSpace(){
     if(numString.slice(-1) == "T"){
         numString = numString.slice(0, -1);
         inputstring = inputstring.slice(0, -1);
-        let popped = operand2.pop()
+        let popped = operand2.pop();
         input.value = inputstring;
-        console.log(operand2)
     }else{
         numString = numString.slice(0, -1);
         inputstring = inputstring.slice(0, -1);
         input.value = inputstring;
-        console.log(num)
     }
 }
 
